fbfc7dd909159d93d195341203449635abcecf8b
Dharmendra Sharma <ds0736038@gmail.com>
2025-07-13 20:30:52 +0530

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// menu-btn
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
+
+
+// home slider
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+// review slider
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+
+// blogs slider
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+}); 
